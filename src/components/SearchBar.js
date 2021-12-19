fefa9c78fe3415a6b6d3adb658d96cafb3c31a13
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-19 09:53:04 -0500

    replace search text with search icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,32 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
 
 export const SearchBar = ({handleChange, handleClick}) => {
   return (
     <Stack className="sidebar"spacing={2}>
-      <h2>Search for a City:</h2>
-      <TextField onChange={e => handleChange(e)} id="outlined-basic" label="City" variant="outlined" />
-      <Button onClick={e => handleClick(e)} variant="outlined">Search</Button>
+      <Typography variant="h5">Search for a City</Typography>
+
+      <Paper
+      component="form"
+      sx={{ p: '2px 4px', 
+      display: 'flex', 
+      alignItems: 'center' }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="City"
+        inputProps={{ 'aria-label': 'City' }}
+        onChange={e => handleChange(e)} 
+      />
+
+        <IconButton onClick={e => { handleClick(e);}} aria-label="search"><SearchIcon /></IconButton>
+      
+      </Paper>
     </Stack>
   )
 }
