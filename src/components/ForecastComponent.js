728c152ec06e93fb63f4766df9f91ce5e27108bb
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-21 18:07:31 -0500

    do not show today in the 5 day forecast

--- src/components/ForecastComponent.js
+++ src/components/ForecastComponent.js
@@ -1,11 +1,14 @@
 import Stack from '@mui/material/Stack';
 import { ForecastCard } from './ForecastCard';
+import dayjs from 'dayjs';
 
 export const ForecastComponent = ({data, i}) => {
   let sortByDate = {};
   const parseData = data => data.forEach(obj => {
     //get date for current object
     const date = obj.dt_txt.split(" ")[0];
+    const d = dayjs(new Date()).format('YYYY-MM-DD');
+    if (d === date) return;
     //get key
     if (!sortByDate[date]) {
       sortByDate[date] = [obj];
